0ed3de672a482b68f7e636d1195403ca77630c86
Gabriel Soares <biel.gbferreira@gmail.com>
2022-01-21 13:38:29 -0300

    pagina home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  providers: [ScreenOrientation]
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private screenOrientation: ScreenOrientation, private platform: Platform) { 
+    this.initializeApp()
+  }
+
+  initializeApp() {
+    if(this.platform.is('cordova')){
+      this.platform.ready().then(()=>{
+       this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+      })
+    }
+  }
+
 }
