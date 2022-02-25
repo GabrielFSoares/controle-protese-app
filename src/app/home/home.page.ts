36319df1f107112c0aec55216344444793cfaab1
Gabriel Soares <biel.gbferreira@gmail.com>
2022-02-25 14:44:25 -0300

    Aplicando filtros

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { Component, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { app } from '../firebaseConfig';
-import { getFirestore, collection, query, getDocs } from "firebase/firestore";
+import { getFirestore, collection, query, getDocs, where } from "firebase/firestore";
 import { ModalController } from '@ionic/angular';
 import { ModalNoteComponent } from '../components/modal-note/modal-note.component';
 
@@ -20,15 +20,53 @@ export class HomePage {
   public patient: string
   public doctor: string
   public provider: string
+  public serie: number
   public date: string
-  public movements: any
-  public docId: any
-  public itens: any
+  public issueDate: string
+  public movements =[]
+  public movement: string
+  public docId = []
+  public itens = []
+  public doctorList = []
+  public providerList = []
+  public loadNote = false
 
   constructor(private modalService: BsModalService, public router: Router, public modalController: ModalController) { }
 
   ngOnInit() {
     this.movementsLoad()
+
+    this.doctorList = [
+      'Marcelo Alonso',
+      'Felipe Magno',
+      'Marcio Teixeira',
+      'Marcio Bistene',
+      'Marcio Wallace',
+      'Ramon Ramalho',
+      'Orido Pinheiro',
+      'Orido Felipe',
+      'Acrysio Peixoto',
+      'Eduardo Sucupira',
+      'Fernando Serra',
+      'Renata Wanick',
+      'Flavia Dantas',
+      'Sílvia Baima',
+      'Roberta Alvares',
+      'Guilherme Miranda',
+      'Ricardo Cunha',
+      'Adriano Medeiros',
+      'José Horácio',
+      'Horácio Gomes',
+      'Renato Monteiro',
+      'Gustavo Merheb',
+      'George Soares',
+      'George Mofoud',
+      'Bruno Herkenhoff',
+      'Ruben Bartz',
+      'Bruno Anastácio'
+    ]
+
+    this.providerList = ['SILIMED', 'POLYTECH', 'MOTIVA', 'MENTOR']
   }
 
   openModal(template: TemplateRef<any>, i: number) {
@@ -56,10 +94,6 @@ export class HomePage {
   async movementsLoad() {
     const q = query(collection(db, "NotaFiscal"))
     const querySnapshot = await getDocs(q)
-    
-    this.movements = []
-    this.docId = []
-    this.itens = []
 
     querySnapshot.forEach((doc) => {
       this.movements.push(doc.data()) 
@@ -75,10 +109,10 @@ export class HomePage {
 
       this.itens.push(arr)
     })
-  }
 
-  openNote(id) {
-    console.log(this.docId[id])
+    if(this.movements.length != 0) {
+      this.loadNote = true 
+    }
   }
 
   async presentModal(i: number) {
@@ -90,5 +124,41 @@ export class HomePage {
       }
     });
     return await modal.present();
+  }
+
+  async test() {
+    const notesRef = collection(db, "NotaFiscal")
+
+    let filter = query(notesRef)
+
+    if(this.noteNumber != undefined && this.noteNumber != null) {
+      filter = query(filter, where("numNota", "==", this.noteNumber))
+    }
+
+    if(this.provider != undefined && this.provider != '') {
+      filter = query(filter, where("fornecedor", "==", this.provider))
+    }
+
+    if(this.patient != undefined && this.patient != '') {
+      filter = query(filter, where("paciente", "==", this.patient))
+    }
+
+    if(this.doctor != undefined && this.doctor != '') {
+      filter = query(filter, where("medico", "==", this.doctor))
+    }
+
+    if(this.movement != undefined && this.movement != '') {
+      filter = query(filter, where("movimentacao", "==", this.movement))
+    }
+
+    /*if(this.serie != undefined && this.serie != null) {
+      filter = query(filter, where("item.0.serie", "==", this.serie))
+    }*/
+
+    const querySnapshot = await getDocs(filter)
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data())
+    })
   }
 }
