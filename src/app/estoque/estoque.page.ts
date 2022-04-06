9c87cc855a169d67f22672a4e57f587e821475b0
Gabriel Soares <biel.gbferreira@gmail.com>
2022-04-06 16:38:42 -0300

    página do estoque

--- src/app/estoque/estoque.page.ts
+++ src/app/estoque/estoque.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { app } from '../firebaseConfig';
+import { getFirestore, collection, query, getDocs, where, orderBy, updateDoc, doc } from "firebase/firestore";
+
+const db = getFirestore(app)
 
 @Component({
   selector: 'app-estoque',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstoquePage implements OnInit {
 
+  public itens = []
+  public loadNote: boolean
+  public serie: number
+
   constructor() { }
 
   ngOnInit() {
+    this.itensLoad()
   }
 
+  async itensLoad() {
+    const notesRef = query(collection(db, "Estoque"))
+    let search = notesRef
+
+    this.itens = []
+
+    if(this.serie != null) {
+      search = query(collection(db, "Estoque"), where("serie", "==", this.serie.toString()))
+    }
+
+    const querySnapshot = await getDocs(search)
+
+    querySnapshot.forEach((doc) => {  
+      this.itens.push(doc.data())
+    }) 
+
+    if(this.itens.length == 0) {
+      this.loadNote = false
+    } else {
+      this.loadNote = true
+    }
+  }
 }
