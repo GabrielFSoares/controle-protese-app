92898cd655442498ab85f09bf0a0f8f022c86893
Your Name <you@example.com>
2022-06-04 20:27:58 -0300

    Versao GIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,24 @@
 import { Component } from '@angular/core';
-//import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
+import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
 import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  //providers: [ScreenOrientation]
+  providers: [ScreenOrientation]
 })
 export class AppComponent {
-  constructor(/*private screenOrientation: ScreenOrientation,*/ private platform: Platform) { 
+  constructor(private screenOrientation: ScreenOrientation, private platform: Platform) { 
     this.initializeApp()
   }
 
   initializeApp() {
-    /*if(this.platform.is('cordova')){
+    if(this.platform.is('cordova')){
       this.platform.ready().then(()=>{
        this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
       })
-    }*/
+    }
   }
 
 }
